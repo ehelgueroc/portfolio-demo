bd4fb74a6fea36219ad5253af2ce0eaee67b2148
Esteban Helguero <esteban.helguero@redk.net>
2021-04-02 21:02:32 +0200

    Added Gatsby template - created projects route and changed index page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function about() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam harum nobis iste deleniti, fuga molestiae sunt! Doloremque dolores aspernatur eveniet enim eaque temporibus quam? Aperiam corporis dolores veritatis alias modi!</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam harum nobis iste deleniti, fuga molestiae sunt! Doloremque dolores aspernatur eveniet enim eaque temporibus quam? Aperiam corporis dolores veritatis alias modi!</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam harum nobis iste deleniti, fuga molestiae sunt! Doloremque dolores aspernatur eveniet enim eaque temporibus quam? Aperiam corporis dolores veritatis alias modi!</p>
+        </div>
+    )
+}
