85bdd797ac9c72e56d98a19468178c7c316a23eb
Esteban Helguero <esteban.helguero@redk.net>
2021-04-09 00:42:32 +0200

    Added the details on the page

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,19 +1,43 @@
+import { graphql } from 'gatsby'
 import Img from 'gatsby-image'
 import React from 'react'
 import Layout from '../components/Layout'
 import { details , featured } from '../styles/project-details.module.css'
 
-export default function ProjectDetails() {
+export default function ProjectDetails({ data }) {
+    console.log(data);
+    const { html } = data.markdownRemark
+    const { title, stack, featureImg } = data.markdownRemark.frontmatter
+    console.log(featureImg);
     return (
         <Layout>
             <div className={details}>
-                <h2>title</h2>
-                <h3>stack</h3>
+                <h2>{ title }</h2>
+                <h3>{ stack }</h3>
                 <div className={featured}>
-                    {/* <Img fluid={} /> */}
+                    <Img fluid={featureImg.childImageSharp.fluid} />
                 </div>
-                {/* <div dangerouslySetInnerHTML={} /> */}
+                <div dangerouslySetInnerHTML={{__html: html}} />
             </div>
         </Layout>
     )
 }
+
+export const query = graphql`
+query ProjectsPage($slug: String) {
+    markdownRemark(frontmatter: {slug: {eq: $slug}}) {
+      html
+      frontmatter {
+        stack
+        title
+        featureImg {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }  
+`
